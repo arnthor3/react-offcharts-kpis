90e5d16282715b9322ed8da594310a945287c148
Arnthor Agustsson <arnthor3@gmail.com>
2017-01-20 00:43:18 +0000

    bring in the basics

---
+++ src/Components/Arc.jsx
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react';
+import { arc } from 'd3-shape';
+import { select } from 'd3-selection';
+import 'd3-transition';
+import * as ch from '../Utils/constants';
+
+export default class Arc extends Component {
+  componentDidMount() {
+    this.renderArc();
+  }
+
+  shouldComponentUpdate() {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.renderChart();
+  }
+
+  animate() {
+
+  }
+
+  draw() {
+
+  }
+
+  renderArc() {
+    const shouldAnimate = (
+      this.props.animationEase || this.props.animationTime
+    );
+    if (shouldAnimate) {
+      this.animate();
+      return;
+    }
+    this.draw();
+  }
+
+  render() {
+    return (
+      <g>
+        <path
+          ref={(c) => { this.path = c; }}
+        />
+      </g>
+    );
+  }
+}
